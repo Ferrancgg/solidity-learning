8742d11931b85797060907d049405799e7e3f823
TuNombreDeUsuario <ferrancgg@gmail.com>
2025-09-02 23:51:37 +0200

    firt contract

--- 01_HelloWorld/HelloWorld.sol
+++ 01_HelloWorld/HelloWorld.sol
@@ -1,1 +1,14 @@
-// Primer contrato en Solidity
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+contract HelloWorld {
+    string public hello;
+
+    constructor() {
+        hello = "Hello World ";
+    }
+
+    function sayHello(string memory _hello) public {
+        hello = _hello;
+    }
+}
